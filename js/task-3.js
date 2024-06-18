0733bbcf6e8d359f20fe5afa7a9686c6bfbcc65a
Tati Shevchenko <tatilunarsnail@gmail.com>
2024-06-18 23:36:13 +0300

    )

--- js/task-3.js
+++ js/task-3.js
@@ -2,6 +2,7 @@ const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
 const inputHandler = (evt) => {
+    
     let text = evt.currentTarget.value.trim();
     textOutput.textContent = text.length > 0 ? text : "Anonymous";
 }
