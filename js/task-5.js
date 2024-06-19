bd81214efda84023b316e4d82bb8f76cd90f6b09
Tati Shevchenko <tatilunarsnail@gmail.com>
2024-06-19 11:49:43 +0300

    Add task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+let box = document.querySelector(".widget");
+let colorName = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", changeColorFunc);
+
+function changeColorFunc() {
+  colorName.textContent = getRandomHexColor();
+  box.style.backgroundColor = `${colorName.textContent}`;
+}
