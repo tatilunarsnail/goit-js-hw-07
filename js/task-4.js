e3814510dc18849fef70d331ab528bf408f3aa56
Tati Shevchenko <tatilunarsnail@gmail.com>
2024-06-20 10:50:03 +0300

    Add basic styles

--- js/task-4.js
+++ js/task-4.js
@@ -1,23 +1,23 @@
 const form = document.querySelector(".login-form");
 
-form.addEventListener("submit", submitFunc);
-
 function submitFunc(evnt) {
-
+    
     evnt.preventDefault();
-
+    
     const email = evnt.target.elements.email.value;
     const password = evnt.target.elements.password.value;
-  
+    
     if (email === "" || password === "") {
         alert("All form fields must be filled in");
     }
-
+    
     const loginData = {};
     loginData.email = email.trim();
     loginData.password = password.trim();
-
+    
     console.log(loginData);
-
+    
     evnt.target.reset();
 }
+
+form.addEventListener("submit", submitFunc);
--- js/task-5.js
+++ js/task-5.js
@@ -1,16 +1,16 @@
+let box = document.querySelector(".widget");
+let colorName = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-let box = document.querySelector(".widget");
-let colorName = document.querySelector(".color");
-const button = document.querySelector(".change-color");
-
-button.addEventListener("click", changeColorFunc);
-
 function changeColorFunc() {
   colorName.textContent = getRandomHexColor();
   box.style.backgroundColor = `${colorName.textContent}`;
 }
+
+button.addEventListener("click", changeColorFunc);
